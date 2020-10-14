5181dcc87b33f28f0a0df62ec3a61a2c5840c8c0
Brovina33ka <brovina.ka@phystech.edu>
2020-10-14 19:09:15 +0300

    changed code for task 1

--- labs/lab1/hw1.py
+++ labs/lab1/hw1.py
@@ -1,4 +1,5 @@
-if __name__ == __main__:
-    arr = [(float(i))**2 if '.' in i else (int(i))**2 for i in input().split()]
-    for i in arr:
-        print(i)
+if __name__ == '__main__':
+	arr = {(float(i))**2 for i in input().split()}
+	arr = list(arr)
+	arr.sort()
+	print(*arr)
